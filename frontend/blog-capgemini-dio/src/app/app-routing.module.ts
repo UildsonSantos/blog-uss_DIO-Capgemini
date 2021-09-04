54321ce9275278bc028dafa6113c4aabf6cac331
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-09-04 18:15:50 -0300

    initial feature implementation to contact us

--- frontend/blog-capgemini-dio/src/app/app-routing.module.ts
+++ frontend/blog-capgemini-dio/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { HomeComponent } from './home/home.component';
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path:'', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent},
+  { path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
--- frontend/blog-capgemini-dio/src/app/app.module.ts
+++ frontend/blog-capgemini-dio/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
