6cc65ae97e404743cf16cd286b4ac97f6d158805
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-09-04 19:25:00 -0300

    update README.md

--- frontend/blog-capgemini-dio/src/app/app-routing.module.ts
+++ frontend/blog-capgemini-dio/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { FeedComponent } from './feed/feed.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
 
 const routes: Routes = [
   { path:'', redirectTo: 'home', pathMatch: 'full'},
